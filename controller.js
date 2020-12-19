a3a86056d72c1322bec6ea980c8fef1776f24318
Luca-Colombo <lucacolo94@gmail.com>
2020-12-19 23:55:57 +0100

    Add file parsing and testing

--- controller.js
+++ controller.js
@@ -1,13 +1,75 @@
-const player = require('play-sound')(opts={});
+// eslint-disable-next-line no-undef
+const player = require('play-sound')((opts = {}));
+const { once } = require('events');
+const fs = require('fs');
+const readline = require('readline');
 
 class Controller {
-    constructor() {
+  constructor(debug = false) {
+    this.debug = debug;
+    this.playing = false;
+  }
 
-    }
+  playSong(songName) {
+    this.playing = true;
+    player.play(`${songName}`, (err) => {
+      if (err) throw err;
+    });
+    this.playing = false;
+  }
 
-    playSong(songName){
-        player.play(`${songName}`, (err) => {if(err) throw err});
+  async testFileParsing(fileName) {
+    // this.playing = true;
+    // const filePath = `songEncoding/${fileName}.csv`;
+
+    // const rl = readline.createInterface({
+    //   input: fs.createReadStream(filePath),
+    //   crlfDelay: Infinity,
+    // });
+
+    // rl.on('line', (line) => {
+    //   console.log(`Line from file: ${line}`);
+    // });
+    // this.playing = false;
+
+    try {
+      const filePath = `songEncoding/${fileName}.csv`;
+
+      this.playing = true;
+
+      const rl = readline.createInterface({
+        input: fs.createReadStream(filePath),
+        crlfDelay: Infinity,
+      });
+
+      const startTime = Date.now();
+
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const line of rl) {
+        while (this.playing) {
+          if (this.debug) console.log(line);
+          const nextStep = line.split(',');
+          const currTime = Date.now() - startTime;
+          if (parseInt(nextStep[0], 10) <= currTime) {
+
+          }
+        }
+      }
+
+      // rl.on('line', (line) => {
+      //   // console.log(`Line from file: ${line}`);
+
+      // });
+
+      await once(rl, 'close');
+
+      console.log('File processed.');
+    } catch (err) {
+      console.error(err);
+    } finally {
+      this.playing = false;
     }
+  }
 }
 
 module.exports = new Controller();
--- test.js
+++ test.js
@@ -6,12 +6,7 @@ const Controller = require('./controller');
 const app = express();
 const port = 3000;
 
-const { Gpio } = require('onoff');
-
 const config = require('./config.json');
-
-const leds = config.pins.map((pin) => new Gpio(pin, 'out'));
-const ligthsOnly = leds.slice(0, 6);
 
 app.use(express.static('public'));
 
@@ -19,14 +14,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/on', (req, res) => {
-  res.json({ status: 'on' });
-  ligthsOnly.forEach((led) => led.writeSync(1));
-});
-app.get('/off', (req, res) => {
-  res.json({ status: 'off' });
-  ligthsOnly.forEach((led) => led.writeSync(0));
-});
 app.get('/play', (req, res) => {
   res.json({ status: 'playing' });
   Controller.playSong('./songs/santatown.mp3');
@@ -46,17 +33,3 @@ app.listen(port, () => {
 //   console.log('Exiting...');
 //   process.exit();
 // }
-
-process.on('SIGINT', () => {
-  console.log('Unexporting leds...');
-  leds.forEach((led) => led.unexport());
-  console.log('Exiting...');
-  process.exit();
-});
-
-process.on('SIGTERM', () => {
-  console.log('Unexporting leds...');
-  leds.forEach((led) => led.unexport());
-  console.log('Exiting...');
-  process.exit();
-});
